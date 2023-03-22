45d9ebfb12911151b6cba550dad7cc99aec53a0c
Keith Chau <wkeith.chau@gmail.com>
2023-03-22 23:22:28 +1030

    Re-organise npm's public registry api method

--- node-dep-tree.js
+++ node-dep-tree.js
@@ -1,15 +1,27 @@
 import fs from "fs";
 
-import { getDependencyTree } from "./helpers.js";
+import { getDependencyTree } from "./dep-from-registry-api.js";
 
-const DESTINATION_FILE = "./dependency-tree.json";
+const DESTINATION_FILE = "api-dep-tree.json";
+const PACKAGES_DIR = "./packages";
 
 if (process.argv.length === 3) {
   const target = process.argv[2];
   if (target.indexOf("@") !== -1) {
     const [packageName, packageVersion] = target.split("@");
+
+    // Ensure packages directory exists
+    const path = `${PACKAGES_DIR}/${target}`;
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path, { recursive: true });
+    }
+
+    // Generate dependency tree based from npm's public registry API
     const depTree = await getDependencyTree(packageName, packageVersion);
-    fs.writeFileSync(DESTINATION_FILE, JSON.stringify(depTree, null, 2));
+    fs.writeFileSync(
+      `${path}/${DESTINATION_FILE}`,
+      JSON.stringify(depTree, null, 2)
+    );
     process.exit();
   }
 }
